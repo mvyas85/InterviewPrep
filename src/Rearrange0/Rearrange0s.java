95bdba12362154dad259405f4c2ce8ae2edf5de1
Manisha <manivyas@cisco.com>
2015-09-18 17:02:32 -0700

    ReArrange 0s

---
+++ src/Rearrange0/Rearrange0s.java
@@ -0,0 +1,58 @@
+package Rearrange0;
+/*
+ * Given an array of ints = [6, 4, 0, 5, 0, 0, 0, 1, 0] move all non zero numbers to the left and zeros to the right. How can you now improve your answer to O(n)? 
+ */
+public class Rearrange0s {
+	
+
+	public static void main(String args[]){
+		int[] myArr = {6, 4, 0,0, 5, 0, 0, 0, 1, 0};
+
+		printArr(myArr);
+		
+		reArrange(myArr);
+System.out.println("After \n");
+		printArr(myArr);
+	}
+	public static void reArrange(int[] arr){
+	
+		int len = arr.length;
+		int i =0,j=len -1;
+		while(i<j){
+	
+			while(i< j ){
+				
+				if(arr[i] == 0)
+					break;
+				i++;
+			}
+			
+			while(j> i){
+				
+				if(arr[j] != 0)
+					break;
+				j--;
+			}
+			if(i==j)
+				break;
+			
+			swap(arr,i,j);
+		}
+	}
+	
+	private static void swap(int[] arr, int i, int j) {
+		System.out.println("\nSwapping arr["+i + "] ="+arr[i]+" arr["+j+ "] ="+arr[j]);
+		int temp = arr[i];
+		arr[i]= arr[j];
+		arr[j] = temp;
+		
+		printArr(arr);
+	}
+	public static void printArr(int[] arr){
+		for(int i=0;i<arr.length;i++){
+			System.out.print(arr[i]+" ");
+		}
+	}
+
+	
+}
--- src/removeComments/RemoveComment.java
+++ src/removeComments/RemoveComment.java
@@ -25,28 +25,34 @@ import java.io.IOException;
 public class RemoveComment {
 	static String eol = System.lineSeparator();
 	public static void main(String args[]){
-//		StringBuilder sb = new StringBuilder();
-//		try {
-//			BufferedReader br = new BufferedReader(new FileReader("myfile.txt"));
-//			
-//			String line = br.readLine() ;
-//			while(line !=null){
-//				sb.append(line);
-//				sb.append(System.lineSeparator());
-//				
-//				line = br.readLine() ;				
-//			}
-//		} catch (FileNotFoundException e) {
-//			e.printStackTrace();
-//		} catch (IOException e) {
-//			e.printStackTrace();
-//		}
-		String str="// int a is here "+eol
-				+ "int  a = 1;"+eol
-				+ "int b = 2 ; // int b here ";
+		StringBuilder sb = new StringBuilder();
+		try {
+			BufferedReader br = new BufferedReader(new FileReader("myfile.txt"));
+			
+			String line = br.readLine() ;	
+			while(line !=null){
+				sb.append(line);
+				sb.append(System.lineSeparator());
+				
+				line = br.readLine() ;		
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+
+		System.out.println(sb.toString());
+		System.out.println("Output file::\n"+removeComments(sb.toString()));
 		
 		
-		System.out.println("Output file::\n"+removeComments(str));
+//		String str="// int a is here "+eol
+//				+ "int  a = 1;"+eol
+//				+ "int b = 2 ; // int b here ";
+//		
+//		
+//		System.out.println("Output file::\n"+removeComments(str));
 	}
 
 		public static String removeComments(String str){
